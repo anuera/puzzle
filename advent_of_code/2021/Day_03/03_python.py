80ba8b4c1d17e2e6b4d5accf9a4e313dc673a977
anuera.fy <anuera.fy@gmail.com>
2022-06-12 19:52:50 +0200

    day 3

--- advent_of_code/2021/Day_03/03_python.py
+++ advent_of_code/2021/Day_03/03_python.py
@@ -4,7 +4,7 @@ import os
 
 
 def open_file():
-    i =  "/test_case.txt"
+    i =  "/input.txt"
     file_path = os.path.dirname(os.path.realpath(__file__)) + i
     with open(file_path,"r") as f:
         return f.read().splitlines()
@@ -20,6 +20,33 @@ def main():
 
 def part_one(source):
     # part one:
+    #print("lines",len(source))
+    
+    gamma = ""
+    epsilon = ""
+
+    
+    for char in (range(len(source[0]))):
+        sum = 0
+        for line in source:
+    
+            sum += int(line[char])
+
+        if sum > (len(source)/2):
+            gamma += "1"
+            epsilon += "0"
+        elif sum < (len(source)/2):
+            gamma += "0"
+            epsilon += "1"
+              
+
+
+    gamma = int(gamma,2)
+    epsilon = int(epsilon,2)
+
+    return(gamma * epsilon)
+
+    
     
     ...
 
